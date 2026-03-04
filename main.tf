2de482c6ce686d476cb1c294370b162fec0a210d
Alexandro Valdez <alexandroivaldez@gmail.com>
2026-03-03 23:57:36 -0600

    Added branch protection to main

--- main.tf
+++ main.tf
@@ -7,3 +7,24 @@ resource "github_repository" "readme_repo" {
   description = "Profile README repo"
   visibility  = "public"
 }
+
+resource "github_branch_default" "default" {
+  repository = github_repository.readme_repo.name
+  branch     = "main"
+}
+
+resource "github_branch_protection" "main" {
+  repository_id = github_repository.readme_repo.node_id
+  pattern       = "main"
+  
+  required_pull_request_reviews {
+    required_approving_review_count = 0
+  }
+
+  required_status_checks {
+    strict = true
+    contexts = []
+  }
+
+  enforce_admins = true
+}
