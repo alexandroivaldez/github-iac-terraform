43c6033ff5427018cc5ea397a0bb20ae2cda9dcc
Alexandro Valdez <alexandroivaldez@gmail.com>
2026-03-24 20:59:51 -0400

    feat: Added new repo

--- main.tf
+++ main.tf
@@ -11,3 +11,10 @@ module "deployment_observatory_repo" {
   repo_name    = "deployment-observatory"
   description  = "Live CI/CD deployment dashboard. Built using Flask, Docker, ECS Fargate and Terraform"
 }
+
+module "k8s_url_shortener_repo" {
+  source       = "./modules/repository"
+  github_token = var.github_token
+  repo_name    = "k8s-url-shortener"
+  description  = "A URL shortener built with FastAPI and deployed on Kubernetes."
+}
