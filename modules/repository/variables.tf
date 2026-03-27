d78bf59a01fed6af0709424bfd35d2487daa7001
Alexandro Valdez <alexandroivaldez@gmail.com>
2026-03-26 21:10:01 -0400

    fix: removed hardcoded Terraform check

--- modules/repository/variables.tf
+++ modules/repository/variables.tf
@@ -4,3 +4,7 @@ variable "description" {}
 variable "visibility" { default = "public" }
 variable "default_branch" { default = "main" }
 variable "required_approving_review_count" { default = 0 }
+variable "status_check_contexts" {
+    type = list(string)
+    default = []
+}
